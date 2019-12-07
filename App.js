cdec353c11fb6afdcd1340ff2c9570877d54c60a
Alexandro Gonçalves Dos Santos <alexandrogonsan@outlook.com>
2019-12-07 15:28:18 -0400

    Send

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.container}>
+				<View style={styles.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+				</View>
+				<View style={styles.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+				</View>
+			</View>
+			<View style={styles.container}>
+				<View style={styles.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+				</View>
+				<View style={styles.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+				</View>
+			</View>
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+});
